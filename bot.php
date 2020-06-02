200645f70b0a9bd1589c48484dc07512568fae07
Sapayev-PC <rich.sapoyev@mail.ru>
2020-06-02 10:49:04 +0500

    bot files

--- bot.php
+++ bot.php
@@ -1,8 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sapayev-PC
- * Date: 02.06.2020
- * Time: 0:23
- */
-echo 'salomlar';
+
+require_once "Telegram.php";
+
+$telegram = new Telegram('1119147401:AAH1EixEagpJEGF-GMp_8Z5sVju1kt5p6A8');
+$data = $telegram->getData();
+$message = $data['message'];
+$text = $message['text'];
+$chat_id = $data['chat']['id'];
+
+if($text = '/start'){
+    $content = [
+      'chat_id' => $chat_id,
+      'text' => $content
+    ];
+    $telegram->sendMessage($content);
+}
