e3703b0161c4327828052b7c03b2629056a3dc31
Sapayev-PC <rich.sapoyev@mail.ru>
2020-06-10 12:14:19 +0500

    pages file

---
+++ Pages.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sapayev-PC
+ * Date: 04.06.2020
+ * Time: 16:29
+ */
+
+class Pages
+{
+    const
+        START = 'start',
+        MAIN = 'main',
+        DISTRICTS = 'districts',
+        SUBJECTS = 'subjects',
+        CENTER = 'center';
+}
